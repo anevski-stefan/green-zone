4879636938af0ebe8b0bc8e581f32ab1342f4f1d
Stefan Anevski <anevskistefan11@gmail.com>
2024-02-14 20:18:19 +0100

    Add additional property for about me text for the user model

--- back-end/src/main/java/com/example/backend/model/User.java
+++ back-end/src/main/java/com/example/backend/model/User.java
@@ -18,9 +18,42 @@ public class User {
     private String email;
     private Integer points = 0;
     private String imagePath;
+    private String aboutMeText;
 
     @ElementCollection
     private List<String> interests;
+
+    public User(String first_name, String second_name, String email, String imagePath, String aboutMeText, List<String> interests) {
+        this.first_name = first_name;
+        this.second_name = second_name;
+        this.email = email;
+        this.imagePath = imagePath;
+        this.aboutMeText = aboutMeText;
+        this.interests = interests;
+    }
+
+    public User(String first_name, String second_name, String email, String imagePath, String aboutMeText) {
+        this.first_name = first_name;
+        this.second_name = second_name;
+        this.email = email;
+        this.imagePath = imagePath;
+        this.aboutMeText = aboutMeText;
+    }
+
+    public User(String first_name, String second_name, String email, String imagePath) {
+        this.first_name = first_name;
+        this.second_name = second_name;
+        this.email = email;
+        this.imagePath = imagePath;
+    }
+
+    public User(String first_name, String second_name, String email, String aboutMeText, List<String> interests) {
+        this.first_name = first_name;
+        this.second_name = second_name;
+        this.email = email;
+        this.aboutMeText = aboutMeText;
+        this.interests = interests;
+    }
 
     public User(String first_name, String second_name, String email, List<String> interests) {
         this.first_name = first_name;
@@ -33,20 +66,5 @@ public class User {
         this.first_name = first_name;
         this.second_name = second_name;
         this.email = email;
-    }
-
-    public User(String first_name, String second_name, String email, String imagePath) {
-        this.first_name = first_name;
-        this.second_name = second_name;
-        this.email = email;
-        this.imagePath = imagePath;
-    }
-
-    public User(String first_name, String second_name, String email, String imagePath, List<String> interests) {
-        this.first_name = first_name;
-        this.second_name = second_name;
-        this.email = email;
-        this.imagePath = imagePath;
-        this.interests = interests;
     }
 }
