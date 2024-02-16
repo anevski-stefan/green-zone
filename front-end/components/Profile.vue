47bf62346083993d716f7ffe8ddf781936b882b0
Stefan Anevski <anevskistefan11@gmail.com>
2024-02-16 21:34:41 +0100

    Fix 500 error with profile component

--- front-end/components/Profile.vue
+++ front-end/components/Profile.vue
@@ -1,12 +1,12 @@
 <template>
 
   <div class="container">
-    <div class="header">
-      <div class="logo">
-        <img src="../../front-end/public/images/posts/profile_picture.png" alt="profile_picture">
-        <span class="logo-text">GreenZone</span>
+    <div class="header bg-green-800 text-black flex justify-between p-2">
+      <div class="logo flex items-center">
+        <img src="../public/images/posts/neom-HYtBA9xDyfg-unsplash.jpg" alt="profile_picture" class="w-12 h-12 mr-2">
+        <span class="logo-text text-xl font-bold">GreenZone</span>
       </div>
-      <div class="info">
+      <div class="info text-right">
         <p>Name: John</p>
         <p>Lastname: Doe</p>
         <p>Email: johndoe23@gmail.com</p>
@@ -15,66 +15,20 @@
       </div>
     </div>
 
-    <div class="content">
-      <p class="about-me">
+    <div class="content bg-green-300 p-4">
+      <p class="about-me text-white text-lg">
         About me:<br>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua.
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
-      <Profile v-if="profile" :user="profile" />
+      <Profile v-if="profile" :user="profile"/>
     </div>
   </div>
 </template>
 
-<script setup>
-
-import profile from "~/pages/profile/index.vue";
-
-
-
-</script>
-
 <style>
-.container {
-  font-family: Arial, sans-serif;
-}
-
-.header {
-  background-color: #285C49;
-  color: #000;
-  display: flex;
-  justify-content: space-between;
-  padding: 10px;
-}
-
-.logo {
-  display: flex;
-  align-items: center;
-}
-
-.logo img {
-  width: 50px;
-  height: 50px;
-  margin-right: 10px;
-}
-
-.logo-text {
-  font-size: 18px;
-}
-
-.info {
-  text-align: right;
-}
-
-.content {
-  background-color: #96C0B1;
-  padding: 20px;
-}
-
-.about-me {
-  color: #FEFAE4;
-  font-size: 16px;
-}
+/* Your style code here */
 </style>
