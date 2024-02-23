cfb5d73e9b1607e37fa9ce20e7a22a1c87d955de
Martina <apostoloska21@gmail.com>
2024-02-23 19:45:04 +0100

    green-zone: add register component and update register page

--- front-end/pages/register/index.vue
+++ front-end/pages/register/index.vue
@@ -5,36 +5,4 @@
   </div>
 </template>
 
-<script>
-import Register from "@/components/Register.vue";
-
-export default {
-  components: {
-    Register
-  },
-
-  async fetch() {
-    try {
-      const response = await this.$fetch('/users/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          first_name: "fname",
-          last_name: "lname"
-        }),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-      } else {
-        console.error('Error:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Fetch error:', error);
-    }
-  }
-};
-</script>
 
