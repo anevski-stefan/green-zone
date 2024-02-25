bdfc1039d6744f6d485039433814548f4b8fd3f0
Stefan Anevski <anevskistefan11@gmail.com>
2024-02-25 21:49:37 +0100

    green-zone: Change the order of the name field in the registration form

--- front-end/components/Register.vue
+++ front-end/components/Register.vue
@@ -3,6 +3,11 @@
     <div class="w-full max-w-md">
       <form @submit.prevent="register" class="shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Name</label>
+          <input v-model="credentials.name" id="name" name="name" type="text" placeholder="Name" required
+                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+        </div>
+        <div class="mb-4">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="username">Username</label>
           <input v-model="credentials.username" id="username" name="username" type="text" placeholder="Username"
                  required
@@ -17,11 +22,6 @@
           <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
           <input v-model="credentials.password" id="password" name="password" type="password" placeholder="Password"
                  required
-                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-        </div>
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Name</label>
-          <input v-model="credentials.name" id="name" name="name" type="text" placeholder="Name" required
                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
         </div>
         <button type="submit"
